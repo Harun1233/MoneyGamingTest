94aab27f53601160959e2706a3bd3b6a10078d30
Harun1233 <av.harunonat@gmail.com>
2022-01-25 15:51:38 +0000

    Reports and screenshots have been added

--- src/test/java/com/MoneyGaming/tests/MoneyGamingTest.java
+++ src/test/java/com/MoneyGaming/tests/MoneyGamingTest.java
@@ -11,15 +11,23 @@ public class MoneyGamingTest extends TestBase{
     RegistrationPage register=new RegistrationPage();
 
     @Test
-    public void test(){
+    public void test1(){
+        //name of the test
+        extentLogger=report.createTest("Money Gaming Test");
 
         home.clickFirsJoin();
-        register.fillTheForm();
-        String actualWarningMessage = register.getWarningMessage();
-        String expectedWarningMessage="‘ This field is required’ ";
+        extentLogger.info("Click to join button on home page");
 
+        register.fillTheForm();
+        extentLogger.info("Fill the form");
+
+        String actualWarningMessage = register.getWarningMessage();
+        String expectedWarningMessage="This field is requiredd";
+
+        extentLogger.info("Verify that the warning message has been displayed under dob dropdown");
         Assert.assertEquals(actualWarningMessage,expectedWarningMessage,"Verify that the warning message is same");
 
+        extentLogger.pass("Money Gaming test has passed");
 
 
 
--- src/test/java/com/MoneyGaming/tests/TestBase.java
+++ src/test/java/com/MoneyGaming/tests/TestBase.java
@@ -3,14 +3,22 @@ package com.MoneyGaming.tests;
 
 
 
+import com.MoneyGaming.tests.utilities.BrowserUtils;
 import com.MoneyGaming.tests.utilities.ConfigurationReader;
 import com.MoneyGaming.tests.utilities.Driver;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class TestBase {
@@ -18,9 +26,33 @@ public class TestBase {
     protected WebDriver driver;
     protected Actions actions;
     protected WebDriverWait wait;
+    protected ExtentReports report;
+    protected ExtentHtmlReporter htmlReporter;
+    protected ExtentTest extentLogger;
+
+
+@BeforeTest
+public void setTestUp(){
+    report=new ExtentReports();
+    String projectPath=System.getProperty("user.dir");
+    String path=projectPath+"/test-output/report.html";
+    htmlReporter=new ExtentHtmlReporter(path);
+    report.attachReporter(htmlReporter);
+    htmlReporter.config().setReportName("MoneyGamingSmokeTest");
+    report.setSystemInfo("Environment", ConfigurationReader.get("url"));
+    report.setSystemInfo("Browser", ConfigurationReader.get("browser"));
+    report.setSystemInfo("OS",System.getProperty("os.name"));
+}
+
+    @AfterTest
+    public void tearsDownTest(){
+    report.flush();
+    }
 
     @BeforeMethod
     public void setUp(){
+
+
         driver= Driver.get();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
@@ -30,7 +62,17 @@ public class TestBase {
     }
 
     @AfterMethod
-    public void tearsDown() throws InterruptedException {
+    public void tearsDown(ITestResult result) throws InterruptedException, IOException {
+
+    if(result.getStatus()==ITestResult.FAILURE){
+        extentLogger.fail(result.getName());
+
+        String screenShotPath= BrowserUtils.getScreenshot(result.getName());
+        extentLogger.addScreenCaptureFromPath(screenShotPath);
+
+        extentLogger.fail(result.getThrowable());
+    }
+
 
         Thread.sleep(2000);
         Driver.closeDriver();
